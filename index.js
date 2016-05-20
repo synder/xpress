32dbda645151abe783361f21fe439bcaa9ffd918
synder <812256717@qq.com>
2016-05-20 10:43:29 +0800

    update example file

--- index.js
+++ index.js
@@ -2,9 +2,8 @@
  * Created by synder on 16/4/23.
  */
 
-var Xpress = require('./lib/Xpress');
-var Router = require('./lib/Router');
+const Xpress = require('./lib/Xpress');
+const Router = require('./lib/Router');
 
 module.exports = Xpress;
 module.exports.Router = Router;
-
