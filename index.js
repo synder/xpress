a7e1a14dcc0e9d01fc726ee14bd04ff48669c972
synder <812256717@qq.com>
2016-06-08 09:47:41 +0800

    add server global monitor

--- index.js
+++ index.js
@@ -4,8 +4,6 @@
 
 const Xpress = require('./lib/Xpress');
 const Router = require('./lib/Router');
-const Requester = require('./lib/Requester');
 
 module.exports = Xpress;
 module.exports.Router = Router;
-module.exports.Requester = Requester;
