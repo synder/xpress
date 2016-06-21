f4d07ba88f87ce50a06dff3002fe242323dad3d8
synder <812256717@qq.com>
2016-06-21 17:07:00 +0800

    add fs tools

--- index.js
+++ index.js
@@ -8,10 +8,12 @@ const Router = require('./server/Router');
 const parser = require('./lib/parser');
 const string = require('./lib/string');
 const validate = require('./lib/validate');
+const fs = require('./lib/fs');
 
 module.exports = Xpress;
 module.exports.Router = Router;
 
+module.exports.fs = fs;
 module.exports.parser = parser;
 module.exports.string = string;
 module.exports.validate = validate;
